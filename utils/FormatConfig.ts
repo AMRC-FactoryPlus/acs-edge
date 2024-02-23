a44b0b676add4675215671b48932a7cb44571e32
Alex Godbehere <alex.godbehere@gmail.com>
2024-02-23 14:57:17 +0000

    Correctly extract endianness

--- utils/FormatConfig.ts
+++ utils/FormatConfig.ts
@@ -5,6 +5,7 @@
 
 import { schemaMetric, sparkplugDataType, sparkplugMetric, sparkplugTemplate } from "../lib/helpers/typeHandler.js";
 export function reHashConf(conf: any) {
+
     conf.deviceConnections?.forEach((devConn: any, i: number) => {
         devConn.devices?.forEach((dev: any, j: number) => {
             dev.pollInt = devConn.pollInt;
@@ -12,15 +13,7 @@ export function reHashConf(conf: any) {
             dev.delimiter = devConn.delimiter;
             conf.deviceConnections[i].devices[j].metrics = [];
             dev.tags?.forEach((tag: schemaMetric) => {
-                const metric: sparkplugMetric = rehashTag(tag);
-                // if (tag.type == "template") {
-                //     (metric.value as sparkplugTemplate).isDefinition = false;
-                //     (metric.value as sparkplugTemplate).templateRef = tag.templateRef;
-                //     (metric.value as sparkplugTemplate).metrics = (metric.value as sparkplugTemplate).metrics.map((metric) => {
-                //         return rehashTag(metric);
-                //     })
-                // }
-                conf.deviceConnections[i].devices[j].metrics.push(metric);
+                conf.deviceConnections[i].devices[j].metrics.push(extractEndianness(tag));
             })
             delete conf.deviceConnections[i].devices[j].tags;
         })
@@ -28,12 +21,14 @@ export function reHashConf(conf: any) {
     return conf;
 }
 
-export function rehashTag(tag: schemaMetric|any): sparkplugMetric {
-    //add some formatting if the tag is string
+function extractEndianness(tag: schemaMetric|any): sparkplugMetric {
+
+    // Store the endianness of the tag, if it has one
+    const endianness = (tag.type.endsWith("BE") ? 4321 : tag.type.endsWith("LE") ? 1234 : null);
+
+    // Remove the endianness from the tag type
     tag.type = tag.type.replace(/[BL]E/g, "");
-    if(tag.type == sparkplugDataType){
-        lowerCaseType(tag.type);
-    }
+
     return {
         name: tag.Name,
         value: tag.value,
@@ -50,18 +45,9 @@ export function rehashTag(tag: schemaMetric|any): sparkplugMetric {
             tooltip: { value: tag.tooltip, type: sparkplugDataType.string },
             documentation: { value: tag.docs, type: sparkplugDataType.string },
             endianness: {
-                value: (tag.type.endsWith("BE") ? 4321 : tag.type.endsWith("LE") ? 1234 : null),
+                value: endianness,
                 type: sparkplugDataType.uInt16
             }
         }
     };
 }
-
-/**
- * Changes the first letter of a string to lower case
- * @param typeString type to alter case
- * @returns type with the first letter lower case 
- */
-var lowerCaseType = (typeString: string) : string => {
-    return typeString.charAt(0).toUpperCase() + typeString.slice(1);
-};
